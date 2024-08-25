62e61ccb2d19ea585a97768ebb6f2bf31d5e0fcd
arijaya <arijaya130@gmail.com>
2024-08-25 20:56:21 +0800

    ensure the wording and conditional is correct

--- src/components/manage/toastSucc.tsx
+++ src/components/manage/toastSucc.tsx
@@ -1,12 +1,20 @@
-export default function ToastSucc(): JSX.Element {
+export default function ToastSucc(props: { message: string[] }): JSX.Element {
   return (
     <div className="flex justify-end">
       <div className="bg-white p-4 rounded-lg">
-        <div className="flex flex-row gap-[1em] items-center">
-          <img src="images/success.png" alt="success" className="w-[1.5em] h-[1.5em]" />
+        <div className="flex flex-row gap-[0.8em]">
+          <img
+            src="images/success.png"
+            alt="success"
+            className="w-[1.1em] h-[1.1em] mt-[0.2em]"
+          />
           <div className="flex flex-col">
-            <p className="md:text-[1em] text-[0.85em] font-[500]">Create Task Successfully</p>
-            <p className="md:text-[0.85em] text-[0.75em] font-[400]">All Done! Your task was successfully created</p>
+            <p className="md:text-[1em] text-[0.85em] font-[500] text-black">
+              {props.message[0]}
+            </p>
+            <p className="md:text-[0.85em] text-[0.75em] font-[400] text-black">
+              {props.message[1]}
+            </p>
           </div>
         </div>
       </div>
